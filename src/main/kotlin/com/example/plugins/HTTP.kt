ca8ed3f941dfc856d233dc3f5b82b373ad4c8e85
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-03-08 18:42:39 +0100

    Fix linter

--- src/main/kotlin/com/example/plugins/HTTP.kt
+++ src/main/kotlin/com/example/plugins/HTTP.kt
@@ -3,10 +3,9 @@ package com.example.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
+import kotlin.io.println
 
 fun Application.configureHTTP() {
-    var MYvarrialg = ""
-
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
